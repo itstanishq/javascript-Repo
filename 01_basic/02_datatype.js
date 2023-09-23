94c00e32f7cc7626f9e49b424466ccc5e2d1032f
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-23 17:33:06 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,14 @@
+let name = "Tanishq";
+let age = 18 ;
+let isLoggedIn = false;
+
+// number  => range
+// bigInt => string
+// boolian => yess or no
+// num => Stand alone value  
+// undefined => 
+// symbol => find uniqueness used in react
+// object
+
+console.log(typeof null); 
+console.log(typeof undefined); 
