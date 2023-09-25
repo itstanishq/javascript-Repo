e23afd1160014d8946f4bf17cb4efbcf92cc58bd
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-25 16:04:10 +0000

    operations added

--- 01_basic/03_datatypecoversion.js
+++ 01_basic/03_datatypecoversion.js
@@ -1,9 +1,9 @@
 let score = "tanishq";
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number (score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33" can easyly convert into number
 // "33abc" this will give you value NaN(not a number) 
@@ -11,7 +11,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 = true
 // 0 = false
@@ -20,5 +20,38 @@ console.log(booleanIsLoggedIn);
 
 let sumNumber = 33;
 let stringNumber = String(sumNumber);
-console.log(sumNumber);
-console.log(typeof sumNumber); 
+// console.log(sumNumber);
+// console.log(typeof sumNumber); 
+
+// operations
+let value = 3;
+let NegValue = - value ; 
+// console.log(NegValue);
+
+let str = "Tanishq";
+let str2 = " Vaishnavi";         // basic operations
+let str3 = str + str2 ; 
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);            // complex operations
+// console.log("1" + 1 + 2 );
+// console.log(2 + 1 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2 , num3 ;
+
+num1 = num2 = num3 =2 + 2;
+
+let gameCounter = 100 ; 
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
