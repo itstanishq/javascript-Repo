f6a3b23ae7797ec21fdc51ec0b3f7e3f910735ef
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-25 16:43:12 +0000

    comperision file added

---
+++ 01_basic/04_comperision.js
@@ -0,0 +1,13 @@
+// console.log(2 > "1");
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);   // it is true bcz of conversion
+
+console.log(undefined == 0);
+console.log(undefined >0);    //it will give false in all condition
+console.log(undefined<0);
+
+// ===
+console.log("2"===2);         //it check datatype also thats why it is false
