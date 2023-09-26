36b82c95f8a4fc5ac30f2fe3ba489f2c9ca95bf7
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-26 17:08:01 +0000

    string file is added

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,33 @@
+const name = "tanishq ";
+const repoCount = 3;
+//. console.log(name + repoCount +" Value");
+
+// console.log(`My name is ${name} and my repocount is ${repoCount}`);
+ //nowdaysvalidformat
+
+ const gameName = new String('tani-sh');
+ // console.log(gameName[0]);
+
+ // console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("a"));
+
+ const newString = gameName.substring(0 , 4);
+ //console.log(newString);
+
+ const anotherSring = gameName.slice(-6 , 3);
+ //console.log(anotherSring);
+
+const someString = "    tani.   "
+// console.log(someString);
+// console.log(someString.trim());
+
+const url = "www.tani01@gmail.com"
+// console.log(url.replace("01","08"))
+
+// console.log(url.includes("tani"));
+
+const bharat = "I-LOVE-MY-INDIA";
+// console.log(bharat.split("-"))
