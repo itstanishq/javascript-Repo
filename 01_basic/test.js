3d5170d293c629ba8b296275637c6a2636cdf8c3
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-26 18:32:08 +0000

    changes

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,4 +1,21 @@
-let a = 23;
-let b = 10 ; 
-let c = a+b;
-console.log(c);
+// stack and heap
+
+//stack -- primitive value
+//heap -- refrence value
+
+let myname = "tanishq";
+let anotherString = myname ;
+anotherString = "vaishnavi";
+
+//console.log(myname);
+//console.log(anotherString);
+
+let user = {
+    email :"xyz@gmail.com",
+    upi :"xyz@ybl"
+}
+let user2 = user ;
+user2.email = "vaish0123@gmail.com";
+
+console.log(user.email);
+console.log(user2.email);
