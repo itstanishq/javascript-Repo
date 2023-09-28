a2f8000c095587997e8edc80668d9ba39d763fa1
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-28 17:29:43 +0000

    maths and number file is added to the repo

---
+++ 01_basic/07_nums_and_math.js
@@ -0,0 +1,37 @@
+const value = 200; 
+//console.log(value);
+
+const profit = new Number(200);
+//console.log(profit);
+
+// console.log(profit.toString().length);      // this property converts number into strings and makes the string option freely to use .
+
+// console.log(profit.toFixed(2));   //to get less precesion value 
+
+const someValue = 23.8966
+//console.log(someValue.toPrecision(3)); // set the precision value of the ammount
+
+const ammount = 10000000;
+// console.log(ammount.toLocaleString('en-IN'));   // give the indian value standered 
+// ----------------------------Maths----------------------------------------\\
+//console.log(Math);
+
+// console.log(Math.abs(-6));   //converts negative value to positive value
+
+// console.log(Math.round(2.3));  // this will give the round off values.
+// console.log(Math.ceil(4.3));   // this will give the max value output.
+// console.log(Math.floor(3.9));  // this will give the min value output.
+// console.log(Math.min(4,2,4,5,6,7)); // get the output of minimum value.
+// console.log(Math.max(2,6,7,8,9));  // get the output of the maximum value.
+// console.log(Math.random);  // this will give random value btw 0 to 1.
+// console.log((Math.random()*10)+1); // this will get the random value btw 1 to 10.
+const min = 10;
+const max = 20;
+console.log(Math.floor((Math.random()*(max-min+1)))+min); 
+
+// this code will get me the value between 10 - 20 //
+
+
+
+
+
