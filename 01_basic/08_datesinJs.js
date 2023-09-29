a1203894f77a3adfb319cb2219fcefa548b9eac1
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-09-29 16:46:40 +0000

    date file added

---
+++ 01_basic/08_datesinJs.js
@@ -0,0 +1,25 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate);    // gives the unreadable format of date
+
+// console.log(myDate.toString());  // give the more readable coverted time 
+
+// console.log(myDate.toDateString());  // give the perfect date and time
+
+// console.log(myDate.toISOString());     // give the date and time in iso format
+
+// console.log(myDate.toJSON());            // same as upper format
+
+// console.log(myDate.toLocaleString('en-IN'));     // give the date and time in the local format
+
+let myCreatedDate = new Date(2023,2,23,5,3);
+ // console.log(myCreatedDate.toLocaleString()); // this will give the user given date output
+
+ let anotherDate = new Date("2023-01-23");  //yy mm dd syntax
+ // console.log(anotherDate.toLocaleString());
+
+ let sepDate = new Date("01-23-2023");
+ // console.log(sepDate.toLocaleString());  // this will give you staderd time of india date .
+
+// will continue from now. //
