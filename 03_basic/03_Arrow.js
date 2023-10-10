86d0d9e5a16870ab0ee8c3c86b77d3f9708c45eb
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-10-10 18:45:23 +0000

    arrow function complete

--- 03_basic/03_Arrow.js
+++ 03_basic/03_Arrow.js
@@ -13,3 +13,39 @@ const user = {
 // user.welcomeMessage()
 
 // console.log(this);
+
+// function  siyaram(){
+//     let username = "tani"
+//     console.log(this.username);   // undefined 
+// }
+// siyaram();
+
+// ==== Declration of arrow function ====. //\
+
+// const coffie = ()=>{
+//     let username = "tani"
+//     console.log(this);  // this can run in arrow function 
+// }
+
+// coffie();
+
+// ====== pure arrow function discussion ======./
+
+// synatx of addition of arrow function
+
+// const add = (num1,num2) => {
+//     return num1 + num2 ; 
+
+// }
+
+// console.log(add(3,4));
+
+// implicit return 
+
+// const add = (num1,num2) => (num1 + num2) ; 
+
+const add = (num1,num2) => ({username:"tani"}); 
+console.log(add());
+
+
+
