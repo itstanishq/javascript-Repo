143360a06c1989cd3191e40d9f67defc83d39b59
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-10-10 19:27:08 +0000

    iifi function added

---
+++ 03_basic/04_iifi.js
@@ -0,0 +1,15 @@
+// IMMIDIATLY INVOKED FUNCTION EXPERASSION
+
+(function chai(){
+    // name iifi
+    console.log(`DB Connected`);     // to stop the pollution from global scope
+})();
+// ()() --- normal function
+
+( ()=>{
+    // unnamed iifi
+    console.log(`DB Connected Two`);
+} )()
+
+
+// normal interview ques 
