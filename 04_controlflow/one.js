5f4e6a8ede6bb5655472e3b0e63db4d4e8557f29
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-10-12 09:07:51 +0000

    changes in control flow file

---
+++ 04_controlflow/one.js
@@ -0,0 +1,29 @@
+// if statement 
+// let temperature = 41
+// if (temperature<50 ) {
+//     console.log("temperature is less than 50");
+// }else{
+//     console.log("temp is more than 50");
+// }
+
+// console.log("exicuted");
+
+// let score = 200;
+// if (score>100) {
+//     let power = "fly";        //scope problem
+//     console.log(`the power is: ${power}`);
+// }
+
+// let balance = 800 ;
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 900");
+    
+// }else{
+//     console.log("less than 1200");
+// }
+
+
