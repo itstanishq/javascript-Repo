3d6ab0d00cb39e77d75623d00e93bcdd2d86ddfc
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-10-17 17:42:31 +0000

    do while loop done

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,21 @@
+// while loop
+// let i  = 0;
+// while (i<=10) {
+//     //console.log(`the value is : ${i}`);
+//     i+=2
+// }
+
+
+// let myArray = ['ironman','superman','khali'];
+// let arr = 0
+// while(arr<=myArray.length){
+//    console.log(`the value is : ${myArray[arr]}`);
+//     arr++
+// }
+
+
+let score = 11;
+do{
+  console.log(`the value is : ${score}`);
+  score++
+}while(score<=10)
