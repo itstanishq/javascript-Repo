5609628017a36ebe6ae37878d86bf42d53d19a5c
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-10-22 18:33:22 +0000

    change in iterations

--- 05_iterations.js/two.js
+++ 05_iterations.js/two.js
@@ -25,3 +25,22 @@ do{
 // loops done tommorow dom 
 
 //TODAY I LEARNRD ABOUT REACT MY FIRST CLASS
+
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+
+for (initialExpression; condition; updateExpression) {
+  // for loop body: statement
+}
+
+for (let i = 1; i <= 10; i++) {    
+  if (i == 5) {
+      break;
+  }
+  console.log(i);
+}
