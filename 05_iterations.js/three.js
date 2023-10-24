87dedd100552d0ac69db957d07049d7767a2e628
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2023-10-24 09:04:05 +0000

    map

---
+++ 05_iterations.js/three.js
@@ -0,0 +1,24 @@
+// for of.  array spacific loop
+let arr = [1,2,3,4,5];
+for (const num of arr) {
+   // console.log(num);
+}
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+ // console.log(`Each char is ${greet}`);
+}
+
+//maps
+const map = new Map;
+map.set("IN" , "INDIA");
+map.set("PAK" , "PAKISTAN");
+map.set("SR" , "SRI LANKA");
+map.set("FR" , "FRANCE");
+map.set("ENG" , "ENGLAND");
+
+console.log(map);
+
+
+
+
